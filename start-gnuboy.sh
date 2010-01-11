855f4d5df07b91acf02c9fcc5761ca4fbc3697e2
Tag234 <tdjones22@gmail.com>
2010-01-11 00:23:43 -0800

    More Path Fixes

--- start-gnuboy.sh
+++ start-gnuboy.sh
@@ -2,5 +2,5 @@
 mount -o remount,rw /
 mkdir -p /media/internal/gnuboy/
 mkdir -p /media/internal/gnuboy/saves/
-cd /media/cryptofs/apps/usr/palm/applications/com.webosinternals.sdlgnuboy
+cd /media/cryptofs/apps/usr/palm/applications/org.webosinternals.sdlgnuboy
 ./sdlgnuboy --scale=2 --density=2 --bind w +up --bind s +down --bind a +left --bind d +right --bind g +start --bind h +select --bind k +b --bind l +a --savedir=/media/internal/gnuboy/saves/ /media/internal/gnuboy/game.gbc
