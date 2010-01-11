fd80773fbb412359e204989346604a38a0e9a764
Tag234 <tdjones22@gmail.com>
2010-01-11 00:03:01 -0800

    Moved /game.gbc and /saves/ to /media/internal/gnuboy/ Modified appinfo.json information

--- start-gnuboy.sh
+++ start-gnuboy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mount -o remount,rw /
-mkdir -p /saves/
+mkdir -p /media/internal/gnuboy/
+mkdir -p /media/internal/gnuboy/saves/
 cd /media/cryptofs/apps/usr/palm/applications/com.tag234.gnuboy
-./sdlgnuboy --scale=2 --density=2 --bind w +up --bind s +down --bind a +left --bind d +right --bind g +start --bind h +select --bind k +b --bind l +a --savedir=/saves/ /game.gbc
+./sdlgnuboy --scale=2 --density=2 --bind w +up --bind s +down --bind a +left --bind d +right --bind g +start --bind h +select --bind k +b --bind l +a --savedir=/media/internal/gnuboy/saves/ /media/internal/gnuboy/game.gbc
